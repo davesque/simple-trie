917d6773532ffbed427681a2606b4e0fd02a6d16
David Sanders <davesque@gmail.com>
2018-04-22 01:49:45 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+extras_require={
+    'dev': [
+        'pytest',
+        'hypothesis',
+        'ipython',
+    ],
+}
+
+setup(
+    name='simple-trie',
+    version='0.1.0-alpha.0',
+    description="""simple-trie: A simple Ethereum trie implementation.""",
+    long_description_markdown_filename='README.md',
+    author='David Sanders',
+    author_email='davesque@gmail.com',
+    url='https://github.com/ethereum/simple-trie',
+    include_package_data=True,
+    extras_require=extras_require,
+    py_modules=['trie'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+)
