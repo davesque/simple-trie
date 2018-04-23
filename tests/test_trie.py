f20919cd57abe7f6f82c808d5ca98902390e866f
David Sanders <davesque@gmail.com>
2018-04-22 22:42:55 -0600

    Updates

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -13,37 +13,29 @@ key_value_pairs = st.tuples(st.binary(max_size=32), st.binary())
 
 
 @given(st.lists(key_value_pairs, max_size=10, unique_by=lambda pair: pair[0]))
-def test_simple_trie_basic_usage_properties(pairs):
+def test_simple_trie_properties(pairs):
     t = SimpleTrie()
 
     assert len(t) == 0
 
     for i, (key, value) in enumerate(pairs):
+        with pytest.raises(KeyError, match='Value not found'):
+            t[key]
+        with pytest.raises(KeyError, match='Value not found'):
+            del t[key]
+
         t[key] = value
+
+        assert t[key] == value
         assert len(t) == i + 1
 
-    for key, value in pairs:
-        assert t[key] == value
+    for i, (key, value) in enumerate(pairs):
         del t[key]
 
+        with pytest.raises(KeyError, match='Value not found'):
+            t[key]
+        with pytest.raises(KeyError, match='Value not found'):
+            del t[key]
+        assert len(t) == len(pairs) - i - 1
+
     assert len(t) == 0
-
-
-def test_simple_trie_get_non_existant_keys():
-    t = SimpleTrie()
-
-    with pytest.raises(KeyError, match='Value not found for key'):
-        t[b'']
-
-    with pytest.raises(KeyError, match='Value not found for key'):
-        t[b'\xff']
-
-
-def test_simple_trie_delete_non_existant_keys():
-    t = SimpleTrie()
-
-    with pytest.raises(KeyError, match='Value not found for key'):
-        del t[b'']
-
-    with pytest.raises(KeyError, match='Value not found for key'):
-        del t[b'\xff']
