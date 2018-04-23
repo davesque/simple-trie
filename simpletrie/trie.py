c3f40894ba4d23d66ec75fcaf8450a197220cacc
David Sanders <davesque@gmail.com>
2018-04-22 22:06:18 -0600

    Remove unused code

--- simpletrie/trie.py
+++ simpletrie/trie.py
@@ -1,6 +1,3 @@
-from eth_utils.crypto import keccak_256 as kec
-from simplerlp import encode as rlp
-
 from .utils import (
     bytes_to_nibbles,
     nibbles_to_bytes,
@@ -28,92 +25,27 @@ class Branch(Node):
     def __setitem__(self, key, value):
         self.nodes[key] = value
 
-    def copy(self):
-        return type(self)(self.nodes[:], self.value)
-
     @property
     def is_empty(self):
         return all(n is None for n in self.nodes) and self.value is None
-
-    def __iter__(self):
-        yield from self.nodes
-        yield self.value
 
     def __repr__(self):
         node_reprs = []
 
         for i, n in enumerate(self.nodes):
+            if n is None:
+                continue
+
             prefix = '  {}: '.format(hex(i)[-1:])
             repr_lines = repr(n).split('\n')
             indented_repr = '\n'.join(prefix + l for l in repr_lines)
 
             node_reprs.append(indented_repr)
 
-        return '{} (\n{}\n)'.format(repr(self.value), '\n'.join(node_reprs))
+        if node_reprs:
+            return '{} (\n{}\n)'.format(repr(self.value), '\n'.join(node_reprs))
 
-
-class Trie:
-    __slots__ = ('_root_hsh', '_db')
-
-    def __init__(self):
-        self._root_hsh = b''
-        self._db = {}
-
-        self[b''] = b''
-
-    def _get(self, hsh, nibs):
-        # Fetch node from db
-        node = self._db[hsh]
-
-        if len(nibs) == 0:
-            # If all nibbles in the key have been traversed, this node should
-            # contain the key's value
-            return node.value
-
-        # If nibbles remain in key, recurse with hash associated with first
-        # remaining nibble.  Consider other remaining nibbles to be
-        # remaining key.
-        return self._get(node[nibs[0]], nibs[1:])
-
-    def _set(self, hsh, nibs, value):
-        try:
-            # Fetch node from db
-            node = self._db[hsh]
-        except KeyError:
-            # If not found in db, default to new branch node
-            node_ = Branch()
-        else:
-            # If found in db, make copy for mutation
-            node_ = node.copy()
-
-        if len(nibs) == 0:
-            # If all nibbles in the key have been traversed, this node should
-            # store the key's value
-            node_.value = value
-        else:
-            # If nibbles remain in key, recurse with hash associated with first
-            # remaining nibble.  Consider other remaining nibbles to be
-            # remaining key.  Re-use value.
-            child_hsh = self._set(node[nibs[0]], nibs[1:], value)
-            node_[nibs[0]] = child_hsh
-
-        hsh_ = kec(rlp(tuple(node_)))
-        self._db[hsh_] = node_
-
-        return hsh_
-
-    def __getitem__(self, key):
-        return self._get(
-            self._root_hsh,
-            bytes_to_nibbles(key),
-        )
-
-    def __setitem__(self, key, value):
-        self._root_hsh = self._set(
-            self._root_hsh,
-            bytes_to_nibbles(key),
-            value,
-        )
+        return repr(self.value)
 
 
 class SimpleTrie:
