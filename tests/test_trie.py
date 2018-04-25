d5df7759626c889603e6e4ff9f1630c783db8501
David Sanders <davesque@gmail.com>
2018-04-25 15:09:51 -0600

    Begin to re-write trie property tests

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -155,23 +155,18 @@ def test_simple_trie_properties(pairs):
 
     # Trie should be empty and contain only root node
     assert len(t) == 0
-    cur_size = 1
-    assert t.size == cur_size
 
     for i, (key, value) in enumerate(pairs):
         # For each key and value added...
 
         # Attempting to look up or delete non-existant key should cause error
-        with pytest.raises(KeyError, match='Value not found'):
-            t[key]
-        with pytest.raises(KeyError, match='Value not found'):
-            del t[key]
+        # with pytest.raises(KeyError, match='Key not found'):
+        #     t[key]
+        # with pytest.raises(KeyError, match='Key not found'):
+        #     del t[key]
 
         # Should be able to set a value for a key
         t[key] = value
-        if key != b'':
-            # Setting anything other than root key should change node count
-            cur_size += 1
 
         # Value should be present at expected key
         assert t[key] == value
@@ -179,28 +174,18 @@ def test_simple_trie_properties(pairs):
         # There should be i + 1 values in trie
         assert len(t) == i + 1
 
-        # There should be expected number of nodes in trie
-        assert t.size == cur_size
+    # for i, (key, value) in enumerate(pairs):
+    #     # Should be able to delete value at key
+    #     del t[key]
 
-    for i, (key, value) in enumerate(pairs):
-        # Should be able to delete value at key
-        del t[key]
-        if key != b'':
-            # Deleting anything other than root key should change node count
-            cur_size -= 1
+    #     # Attempting to look up or delete non-existant key should cause error
+    #     with pytest.raises(KeyError, match='Value not found'):
+    #         t[key]
+    #     with pytest.raises(KeyError, match='Value not found'):
+    #         del t[key]
 
-        # Attempting to look up or delete non-existant key should cause error
-        with pytest.raises(KeyError, match='Value not found'):
-            t[key]
-        with pytest.raises(KeyError, match='Value not found'):
-            del t[key]
-
-        # There should be one less value in trie after deletion
-        assert len(t) == len(pairs) - i - 1
-
-        # There should be expected number of nodes in trie
-        assert t.size == cur_size
+    #     # There should be one less value in trie after deletion
+    #     assert len(t) == len(pairs) - i - 1
 
     # Trie should be empty and contain only root node
-    assert len(t) == 0
-    assert cur_size == 1
+    # assert len(t) == 0
